d81e40c3dfd8e260db04aeb94400e3ba38e4fc0a
Jenn Franco <jfranco376@gmail.com>
2022-08-13 09:22:05 -0500

    bullets 1-4 complete

--- src/Router.js
+++ src/Router.js
@@ -8,10 +8,20 @@ to the About component.
 Remember to export the component at the end
 */
 
-import React from 'react'
-import { Routes, Route } from 'react-router'
+import React from "react";
+import { Routes, Route } from "react-router";
 // Write component imports here //
-
-
+import Home from "./components/Home";
+import About from "./components/About";
 
 // Start Router function here //
+export default function Router() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
